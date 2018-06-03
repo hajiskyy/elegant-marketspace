c3ddfe3514fd29d6865d23fcadcfd5de486f910c
Haji Mohammed <32811326+hajiskyy@users.noreply.github.com>
2018-06-03 09:29:15 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://hajisky:1234@ds263639.mlab.com:63639/elegante',
-  secret: 'mysecret'
+  database: 'mongodb://*****************************************',
+  secret: '***********'
 }
